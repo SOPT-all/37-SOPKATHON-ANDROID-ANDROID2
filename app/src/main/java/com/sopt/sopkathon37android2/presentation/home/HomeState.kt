b9401c11809f5c5d79404c6d9b0c60a20abf3a9c
sohee <144779368+sohee6989@users.noreply.github.com>
2025-11-23 05:04:18 +0900

    [feat/#18] 홈 화면 구현

--- app/src/main/java/com/sopt/sopkathon37android2/presentation/home/HomeState.kt
+++ app/src/main/java/com/sopt/sopkathon37android2/presentation/home/HomeState.kt
@@ -1,11 +1,14 @@
 package com.sopt.sopkathon37android2.presentation.home
 
 import com.sopt.sopkathon37android2.presentation.home.model.HomeTab
+import com.sopt.sopkathon37android2.presentation.home.model.VoteUiModel
 
 data class HomeState(
     val userId: Long = 0,
     val nickname: String = "",
-    val isSelected: Boolean = false,
     val selectedTab: HomeTab = HomeTab.ISSUE,
-    val isActivated: Boolean = false
+    val isActivated: Boolean = false,
+    val isIssueSelected: Boolean = true,
+    val isVoteSelected: Boolean = false,
+    val voteList : List<VoteUiModel> = emptyList()
 )
---
+++ app/src/main/java/com/sopt/sopkathon37android2/presentation/home/component/HomeVote.kt
@@ -0,0 +1,118 @@
+package com.sopt.sopkathon37android2.presentation.home.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import com.example.sopkathon37android2.R
+import com.sopt.sopkathon37android2.core.designsystem.ui.theme.SopkathonTheme
+
+@Composable
+fun HomeVote(
+    detailGroupTag: String,
+    tagType: TagType,
+    studentCouncil: Boolean,
+    voteTotalNumber: Int,
+    detailTitle: String,
+    detailGroup: String,
+    onVoteClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val displayedTitleText = remember(detailTitle) {
+        if (detailTitle.length > 18){
+            detailTitle.take(18) + "…"
+        } else {
+            detailTitle
+        }
+    }
+
+    val displayedDetailGroupText = remember(detailGroup) {
+        if (detailGroup.length > 20) {
+            detailGroup.take(20) + "…"
+        } else {
+            detailGroup
+        }
+    }
+
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(8.dp))
+            .background(color = SopkathonTheme.colors.white)
+            .clickable(onClick = onVoteClick)
+            .padding(horizontal = 20.dp)
+            .padding(top = 17.dp, bottom = 21.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            HomeTagChip(
+                text = detailGroupTag,
+                type = tagType
+            )
+
+            if (studentCouncil) {
+                Spacer(modifier = Modifier.weight(1f))
+
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_pin),
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+
+        }
+
+        Spacer(modifier = Modifier.height(9.dp))
+
+        Text(
+            text = displayedTitleText,
+            color = SopkathonTheme.colors.black,
+            style = SopkathonTheme.typography.title.sb_16
+        )
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Text(
+            text = displayedDetailGroupText,
+            color = SopkathonTheme.colors.gray04,
+            style = SopkathonTheme.typography.caption.m_12
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(2.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_vote),
+                contentDescription = null,
+                modifier = modifier.size(24.dp)
+            )
+
+            Text(
+                text = "${voteTotalNumber}명이 투표했어요",
+                color = SopkathonTheme.colors.black,
+                style = SopkathonTheme.typography.caption.m_12
+            )
+        }
+    }
+}
