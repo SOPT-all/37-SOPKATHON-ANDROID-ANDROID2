f472fb5328f97b6ea3f36ac751d89cc8ba26989d
SHC <hcshin0717@naver.com>
2025-11-22 22:20:12 +0900

    [add/#2] : Color 설정

--- app/src/main/java/com/sopt/sopkathon37android2/core/designsystem/ui/theme/Color.kt
+++ app/src/main/java/com/sopt/sopkathon37android2/core/designsystem/ui/theme/Color.kt
@@ -1,11 +1,34 @@
 package com.example.sopkathon37android2.ui.theme
 
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val White = Color(0xFFFFFFFF)
+val Gray100 = Color(0xFFF4F4F4)
+val Gray200 = Color(0xFFDADADA)
+val Gray300 = Color(0xFF949494)
+val Gray400 = Color(0xFF767676)
+val Gray600 = Color(0xFF6A6A6A)
+val Gray900 = Color(0xFF141414)
+val Black = Color(0xFF000000)
+val Black_50 = Color(0xFF000000).copy(alpha = 0.5f)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BrandRed = Color(0xFFDF080D)
+
+@Immutable
+data class Colors(
+    val white: Color = White,
+    val black: Color = Black,
+
+    val brandRed: Color = BrandRed,
+
+    val gray100: Color = Gray100,
+    val gray200: Color = Gray200,
+    val gray300: Color = Gray300,
+    val gray400: Color = Gray400,
+    val gray600: Color = Gray600,
+    val gray900: Color = Gray900,
+    val black50: Color = Black_50,
+)
+
+val defaultColors = Colors()
