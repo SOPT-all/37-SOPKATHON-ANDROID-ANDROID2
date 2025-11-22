a24877ef331fff76d9a15da93d4204b6c7a95fa3
Kyu hyunSung <dmp100@naver.com>
2025-11-23 08:10:38 +0900

    [FIX/#25].  Viewmodel fix,. app icon fix

--- app/src/main/java/com/sopt/sopkathon37android2/data/dto/response/IssueResponseDto.kt
+++ app/src/main/java/com/sopt/sopkathon37android2/data/dto/response/IssueResponseDto.kt
@@ -4,6 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class IssueResponseDto(
+    val issueId: Int,
     val title: String,
     val college: String,
     val recommendCount: Int,
