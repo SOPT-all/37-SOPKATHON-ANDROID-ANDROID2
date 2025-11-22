88dab6499847aa56d0a98c75a46811e23a1214db
Kyu hyunSung <dmp100@naver.com>
2025-11-23 03:52:58 +0900

    [FIX/#13] : Chip,Banner Depth 수정

--- app/src/main/java/com/sopt/sopkathon37android2/presentation/home/component/HomeBanner.kt
+++ app/src/main/java/com/sopt/sopkathon37android2/presentation/home/component/HomeBanner.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,20 +21,17 @@ import com.example.sopkathon37android2.R
 
 @Composable
 fun HomeBanner() {
-    Card(
+
+    Image(
+        painter = painterResource(id = R.drawable.ex_issue_image),
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
         modifier = Modifier
             .padding(16.dp)
             .fillMaxWidth()
-            .height(130.dp),
-        shape = RoundedCornerShape(8.dp)
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.ex_issue_image),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxSize()
-        )
-    }
+            .height(130.dp)
+            .clip(RoundedCornerShape(8.dp)),
+    )
 }
 
 
--- app/src/main/java/com/sopt/sopkathon37android2/presentation/home/component/HomeTagChip.kt
+++ app/src/main/java/com/sopt/sopkathon37android2/presentation/home/component/HomeTagChip.kt
@@ -1,5 +1,6 @@
 package com.sopt.sopkathon37android2.presentation.home.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
@@ -9,6 +10,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,31 +18,33 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sopt.sopkathon37android2.core.designsystem.ui.theme.SopkathonTheme
 
-enum class TagType {
-    ALL, MY, OTHER
-}
+enum class TagType { ALL, MY, OTHER }
+
 
 @Composable
 fun HomeTagChip(
     text: String,
     type: TagType = TagType.OTHER
 ) {
-    Surface(
-        color = when (type) {
-            TagType.ALL -> SopkathonTheme.colors.tagAll
-            TagType.MY -> SopkathonTheme.colors.tagMy
-            TagType.OTHER -> SopkathonTheme.colors.tagOther
-        },
-        shape = RoundedCornerShape(99.dp)
-    ) {
-        Text(
-            text = text,
-            style = SopkathonTheme.typography.caption.m_10,
-            color = SopkathonTheme.colors.gray05,
-            modifier = Modifier.padding(horizontal = 8.dp, vertical = 3.dp)
-        )
+    val backgroundColor = when (type) {
+        TagType.ALL -> SopkathonTheme.colors.tagAll
+        TagType.MY -> SopkathonTheme.colors.tagMy
+        TagType.OTHER -> SopkathonTheme.colors.tagOther
     }
+
+    val textColor = SopkathonTheme.colors.gray05
+
+    Text(
+        text = text,
+        style = SopkathonTheme.typography.caption.m_10,
+        color = textColor,
+        modifier = Modifier
+            .clip(RoundedCornerShape(99.dp))
+            .background(backgroundColor)
+            .padding(horizontal = 8.dp, vertical = 3.dp)
+    )
 }
+
 
 @Preview(showBackground = true)
 @Composable
